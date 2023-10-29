1be788372861721fbbc05c8d1a612970d1d8cd77
Alex Alexeev <work2alex@gmail.com>
2023-10-29 23:21:17 +0100

    test: add integration tests for most queries and mutations

--- src/schema.ts
+++ src/schema.ts
@@ -4,18 +4,17 @@ import { fileURLToPath } from 'url';
 
 import * as types from './graphql';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(filename);
 
 export const schema = makeSchema({
   types,
   outputs: {
-    typegen: join(__dirname, '..', 'nexus-typegen.ts'),
-    schema: join(__dirname, '..', 'schema.graphql'),
+    typegen: join(_dirname, '..', 'nexus-typegen.ts'),
+    schema: join(_dirname, '..', 'schema.graphql'),
   },
   contextType: {
-    // 1
-    module: join(__dirname, './context.ts'),
+    module: join(_dirname, './context.ts'),
     export: 'Context',
   },
 });
