454399612d7363b449245053f38977bfdfc57ec3
Alex Alexeev <work2alex@gmail.com>
2023-10-29 18:28:25 +0100

    feat: add in memory db and main query

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 import { server } from './server';
+import { context } from './context';
 
 const { url } = await startStandaloneServer(server, {
-  context: async ({ req }) => ({ token: req.headers.token }),
+  context: async ({ req }) => ({ token: req.headers.token, ...context }),
   listen: { port: 4000 },
 });
 
